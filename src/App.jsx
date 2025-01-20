b5b8996e74be2b0585fa025775ebd17e70bfe6c1
G. Ismael Rivera M <tazrimg.mx@gmail.com>
2025-01-19 22:37:20 -0600

    pruebas con rutas

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {  Route, Routes } from "react-router-dom";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
+  const [value, setValue] = useState("");
       
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -16,11 +17,24 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+
+  // Leer el valor inicial de localStorage cuando el componente se monte
+  useEffect(() => {
+    const storedValue = localStorage.getItem("myValue");
+    if (storedValue) {
+      setValue(storedValue); // Recupera el valor
+    }
+  }, []);
+
+   // Guardar el valor en localStorage cuando cambie
+   useEffect(() => {
+    localStorage.setItem("myValue", value);
+  }, [value]);
  
   return (
     <>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler}  />
+      <Header onShowCart={showCartHandler}  isLoggedIn = { value }/>
       <main>
         <Routes>
           <Route index element={<Login />} />
